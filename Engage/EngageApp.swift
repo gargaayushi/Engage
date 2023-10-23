a44693f4a21ccde32f15890f456c3be050223d1e
Aayushi Garg <aayushigarg17@gmail.com>
2023-10-23 15:25:34 -0700

    Initial Commit

---
+++ Engage/EngageApp.swift
@@ -0,0 +1,17 @@
+//
+//  EngageApp.swift
+//  Engage
+//
+//  Created by Aayushi Garg on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct EngageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
